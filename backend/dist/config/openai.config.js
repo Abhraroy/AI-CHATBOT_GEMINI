73ccac4d705a961db22fd496f5ce949ec7590d39
Abhradip Roy <abhra0312@gmail.com>
2025-04-09 02:22:10 +0530

    chatbot updated

--- backend/dist/config/openai.config.js
+++
@@ -1,8 +0,0 @@
-// import { Configuration } from "openai"
-// export const configureopenai = ()=>{
-//     const config = new Configuration({
-//         apiKey:process.env.OPEN_AI_SECRET,
-//         organization:process.env.OPEN_AI_ORG_KEY
-//     })
-//     return config
-// }
--- backend/dist/index.js
+++
@@ -1,8 +0,0 @@
-import app from './app.js';
-import { databaseconnect } from './DB/Db_Connection.js';
-const PORT = process.env.PORT;
-databaseconnect().then(() => {
-    app.listen(PORT, () => {
-        console.log("your server is runing");
-    });
-}).catch((err) => console.log(err));
