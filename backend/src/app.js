48413609cb11002984f8c61a9922e0ecbe942e1d
Abhradip Roy <abhra0312@gmail.com>
2025-04-09 02:55:42 +0530

    chatbot updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ var cookie_parser_1 = require("cookie-parser");
 var cors_1 = require("cors");
 (0, dotenv_1.config)();
 var app = (0, express_1.default)();
-app.use((0, cors_1.default)({ origin: "effortless-starburst-95d285.netlify.app", credentials: true }));
+app.use((0, cors_1.default)({ origin: "https://effortless-starburst-95d285.netlify.app", credentials: true }));
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)(process.env.COOKIE_SECRET));
 app.use("/api/v1/", router_js_1.default);
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,7 @@ import cors from "cors"
 config()
 const app = express()
 
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({origin:"https://effortless-starburst-95d285.netlify.app",credentials:true}))
 app.use(express.json())
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use("/api/v1/",appRouter)
